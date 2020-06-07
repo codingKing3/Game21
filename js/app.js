7b2565077d5f334cf07b1ddf38c40024f38542e0
King J <king3john@gmail.com>
2020-06-07 11:59:45 -0400

    html add-on

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+   //alert("im in"); 
    $("#twentyOne").css("color","yellow"); 
    $("#twentyOne").show('slow');
    $("#twentyOne").css("text-decoration","underline");
@@ -9,6 +10,7 @@ let deal = document.getElementById('playBtn');
 let cardplayed = [1,2,3,4,5,6,7,8,9,10,11];
 let showMyCard = document.getElementById("card1");
 let player1 = document.querySelector("#player1");
+let drawPlayer1 = document.querySelector("#cardPlayed1");
 let total1 = 0;
 
 // will get random card number and add card value to total amount
@@ -16,6 +18,7 @@ deal.addEventListener("click",function(){
     //showMyCard.innerHTML="<img src = 'images/AC.jpg'>";
     randomNum = Math.floor((Math.random()*11)+1);
     getNumber =cardplayed[randomNum-1];
+    drawPlayer1.innerHTML = getNumber;
     total1 = total1 + getNumber;
     if(total1 < 22){
     showMyCard.innerHTML = total1;
@@ -23,6 +26,8 @@ deal.addEventListener("click",function(){
      else if (total1 > 21){
          showMyCard.innerHTML = "BUSTED";
      }
+
+
 });
 
 // will allow player one to freeze accepting cards before busting out over 21
