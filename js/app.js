d409cf07d8c1d87748dd28884481ceda2086a542
King J <king3john@gmail.com>
2020-07-28 22:34:14 -0400

    adding cards

--- js/app.js
+++ js/app.js
@@ -1,41 +1,26 @@
-$(document).ready(function () {
-    //alert("im in"); 
+$(document).ready(function () {   
     $("#twentyOne").css("color", "yellow");
-    //$("#twentyOne").show('slow');
     $("#twentyOne").css("text-decoration", "underline");
 
 
-// on play button click check for input in name-space
-// if no-name show drop down
-// if set name min length
-// link to player page : upgrade=>later use ajax link
-// get player name: replace player 1 with player name
+ // on play button click check for input in name-space
+ // if no-name show drop down
+ // if set name min length
+ // link to player page : upgrade=>later use ajax link
+ // get player name: replace player 1 with player name
 
 
-//window.onload = function(){
-
-function addName(){
-    //let name = [];
-    let input_name = document.getElementById('e_name').value
-   /* let playerName ={
-        name: input_name
-    };
-    
-    name.push(playerName);
-    pName =document.getElementById('e_name').value; */
-    sessionStorage.setItem("name",input_name);
-     
-  }
+ //window.onload = function(){
 
   // storing name to local storeage 
-// go to next page 
+ // go to next page 
   
-  let playing = document.getElementById('play');
-  playing.addEventListener("click",function() {
-        addName();
-       // person = sessionStorage.getItem("name")
-        //alert(person); 
-      location.href = "public/gameBoard.html";    
+ // let playing = document.querySelector('play');
+  $("#play").on("click",function(){
+        //addName();
+        let input_name = document.getElementById('e_name').value;
+        sessionStorage.setItem("name",input_name);         
+        location.href = "public/gameBoard.html";    
   });     
 
 });
--- js/custom.js
+++ js/custom.js
@@ -1,63 +1,70 @@
-$(document).ready(function (){
-let deal = document.getElementById('playBtn');
+$(document).ready(function () {
+    let deal = document.getElementById("playBtn");
+    let cardplayed = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    let showMyCard = document.getElementById("card1");
+    let player1 = document.querySelector("#player1");
+    let drawPlayer1 = document.getElementById("cardPlayed1");
+    let player1Hold = document.getElementById("freeze1");
+    //let pName = document.getElementById("draw1");
+    let show = document.getElementById("showCard");
+    let total1 = 0;
+    let hold = false;
 
-let cardplayed = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-let showMyCard = document.getElementById("card1");
-let player1 = document.querySelector("#player1");
-let drawPlayer1 = document.getElementById("cardPlayed1");
-let player1Hold = document.getElementById("freeze1");
-let pName = document.getElementById("draw1");
-let total1 = 0;
-let hold = false;
+    // add player name
+    let personName = sessionStorage.getItem("name");
+    //pName.innerHTML = personName + ":   ";
+    $('#draw1').html(personName + ":   ");
 
-// add player name
-pName.innerHTML = sessionStorage.getItem("name")+":  ";
+    //let c_played = "../images/card_deck/JPEG/";
+    //show.src = "../images/card_deck/JPEG/QD.jpg"; //c_played + 'QD.jpg';
 
+    // will get random card number and add card value to total amount
+    //document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+    $("#freeze1").on('click', function () {
+        let iHoldP1 = document.getElementById("draw1");
+        let noCardP1 = document.getElementById("cardPlayed1");
 
-// will get random card number and add card value to total amount
-//document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-player1Hold.addEventListener("click", function(){
- let iHoldP1 = document.getElementById("draw1");
- let noCardP1 = document.getElementById("cardPlayed1");
+        hold = true;
+        iHoldP1.innerHTML = "HOLD";
+        noCardP1.innerHTML = "";
+    });
 
- hold = true;
- iHoldP1.innerHTML = "HOLD";
- noCardP1.innerHTML = ""; 
- 
-});
+    $("#playBtn").on('click', function () {
+        //console.log('this is:',hold);
+        //showMyCard.innerHTML="<img src = 'images/AC.jpg'>";
+        //let show = document.getElementById("showCard");
+        randomNum = Math.floor(Math.random() * 11 + 1);
+        console.log(randomNum);
+        getNumber = cardplayed[randomNum - 1];
+        drawPlayer1.innerHTML = "  " + getNumber;
 
-deal.addEventListener("click", function () {
-    //console.log('this is:',hold);    
-    //showMyCard.innerHTML="<img src = 'images/AC.jpg'>";
-    randomNum = Math.floor((Math.random() * 11) + 1);
-    console.log(randomNum);
-    getNumber = cardplayed[randomNum - 1];
-    drawPlayer1.innerHTML = "  " + getNumber;
-    
-    total1 = total1 + getNumber;
-    if (hold === true){
-       console.log("player one Hold");
-    }
-    else if (total1 < 22) {
-        showMyCard.innerHTML = total1;
-    } else if (total1 > 21) {
-        let vanish = document.getElementById("draw1");
-        showMyCard.classList.add("busted");
-        showMyCard.innerHTML = "BUSTED";
-        drawPlayer1.classList.add("stop");
-        vanish.classList.add("stop");
+        total1 = total1 + getNumber;
+        if (hold === true) {
+            console.log("player one Hold");
+            showMyCard.innerHTML = "Hold";
+            drawPlayer1.classList.add("stop");
+        } else if (total1 < 22) {
+            showMyCard.innerHTML = total1;
+        } else if (total1 > 21) {
+            let vanish = document.getElementById("draw1");
+            showMyCard.classList.add("busted");
+            showMyCard.innerHTML = "BUSTED";
+            //let c_played = "../images/card_deck/JPEG/";
+            show.src = "../images/card_deck/JPEG/QD.jpg"; //c_played + 'QD.jpg';
+            $("#showCard").html(show);
 
-    }
-    // add else if total1 === 21  you win
-    // need to determine what happen if win
-    // need to code to determine who is close to 21 to win
+            console.log(show.src);
+            // $('#image').html(img);
+            // drawPlayer1.classList.add("stop");
+            // vanish.classList.add("stop");
+        }
+        // add else if total1 === 21  you win
+        // need to determine what happen if win
+        // need to code to determine who is close to 21 to win
+    });
 
-
-});
-
-// will allow player one to freeze accepting cards before busting out over 21
-player1.addEventListener("click", function () {
-    let vanish = document.getElementById("cardPlayed1");
-
-  });
+    // will allow player one to freeze accepting cards before busting out over 21
+    $("#player1").on('click', function () {
+        let vanish = document.getElementById("cardPlayed1");
+    });
 }); // end of onload
